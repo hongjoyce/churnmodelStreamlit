66029b24d152803c63ec11ce4712623c9605d735
hongjoyce <hongjoycejiang@gmail.com>
2023-01-13 15:19:41 -0600

    minor bug

--- churnmodel_new.py
+++ churnmodel_new.py
@@ -85,7 +85,7 @@ if st.button('Predict Churn'):
     print(query)
     prediction = str(round(model.predict_proba(query)[0][-1], 3))
     
-    st.subheader("The churn probability of this customer next month is " + prediction)
+    st.subheader("Probability of churn is" + prediction)
 
     shap.initjs()
 
